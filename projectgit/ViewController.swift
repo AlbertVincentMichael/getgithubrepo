e837f66bdd94df49df46c87245a1fa6e794731f5
AlbertVincentMichael <albert@dellainfotech.com>
2022-07-14 15:53:33 +0530

    Initial Commit

---
+++ projectgit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projectgit
+//
+//  Created by albert Michael on 14/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
